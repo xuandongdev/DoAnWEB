ec4f8114288e34bf5634d7240974b6e471f0f033
xuandongdev <xuandong.workspace@gmail.com>
2024-03-19 16:29:36 +0700

    add signup button

--- login/login.js
+++ login/login.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     var exploreBtn = document.getElementById("exploreBtn");
     if(exploreBtn) {
-      exploreBtn.addEventListener("click", function() {
-        window.location.href = "../product.html";
-      });
+        exploreBtn.addEventListener("click", function() {
+            window.location.href = "../product.html";
+        });
     }
-  });
-  
+
+    var signupBtn = document.getElementById("signupBtn");
+    if(signupBtn) {
+        signupBtn.addEventListener("click", function() {
+            console.log("Button Clicked")
+            window.location.href = "../sign_Up.html";
+        });
+    }
+});
+
   const title = document.querySelector('.title h1')
   const letter = 'Welcome!'
   var index = 0
