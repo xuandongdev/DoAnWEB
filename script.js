d9266693817fc93e856b2e3f04f0c48ce3b50ec9
xuandongdev <xuandong.workspace@gmail.com>
2024-03-23 21:58:47 +0700

    update slide & img slide

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let images = document.querySelectorAll(".slide-content-top img");
 
-    let currentIndex = 0;
-    const maxIndex = images.length - 1;
+  var slideIndex = 0;
+  showSlides();
 
-    function slideToNext() {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateSlide();
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    var slideTexts = document.querySelectorAll('.slide-content-left-bottom li');
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
     }
-
-    function updateSlide() {
-        images.forEach((img, index) => {
-            let translateValue = (index - currentIndex) * 100;
-            img.style.transform = `translateX(${translateValue}%)`;
-        });
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
     }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    updateActiveText(slideIndex - 1, slideTexts);
+    setTimeout(showSlides, 1000); // Change image every 2 seconds
+  }
 
-    // Tự động lướt sau mỗi khoảng thời gian
-    setInterval(slideToNext, 3000);
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
-    // Xử lý sự kiện khi click vào hình ảnh
-    images.forEach((img, index) => {
-        img.addEventListener("click", function() {
-            currentIndex = index;
-            updateSlide();
-        });
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll(".slide-content-top a");
-    const totalSlides = slides.length;
-    let currentSlideIndex = 0;
-  
-    function showSlide(index) {
-      slides.forEach(slide => slide.style.transform = `translateX(-${index * 100}%)`);
+  function updateActiveText(index, texts) {
+    for (var i = 0; i < texts.length; i++) {
+      if (i === index) {
+        texts[i].classList.add('active');
+      } else {
+        texts[i].classList.remove('active');
+      }
     }
-  
-    document.querySelector(".bxs-chevron-left").addEventListener("click", function() {
-      currentSlideIndex = (currentSlideIndex - 1 + totalSlides) % totalSlides;
-      showSlide(currentSlideIndex);
-    });
-  
-    document.querySelector(".bxs-chevron-right").addEventListener("click", function() {
-      currentSlideIndex = (currentSlideIndex + 1) % totalSlides;
-      showSlide(currentSlideIndex);
-    });
-  });
-      
+  }
