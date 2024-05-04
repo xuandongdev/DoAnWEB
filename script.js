0e2a0d928ba1670876eedb981678d45ff83e9832
XuanDongDev <xuandong.workspace@gmail.com>
2024-05-04 18:20:06 +0700

    update library slide

--- script.js
+++ script.js
@@ -1,74 +1,21 @@
-// START SLIDE
-//-------CLICK SLIDE-------
-let slideIndex = 1;
-showSlides(slideIndex);
-// Next/previous controls
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-// Thumbnail image controls
-function currentSlide(n) {
-    slideIndex = n;
-    showSlides(slideIndex);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("activeCap");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].classList.remove("active");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].classList.add("active");
-}
-//-------AUTO SLIDE-------
-let slideIndexAuto = 0;
-showSlidesAuto();
-
-function showSlidesAuto() {
-    let i;
-    let slidesAuto = document.getElementsByClassName("mySlides");
-    let dotsAuto = document.getElementsByClassName("activeCap");
-    for (i = 0; i < slidesAuto.length; i++) {
-        slidesAuto[i].style.display = "none";
-        dotsAuto[i].classList.remove("active");
-    }
-    slideIndexAuto++;
-    if (slideIndexAuto > slidesAuto.length) {
-        slideIndexAuto = 1;
-    }
-    slidesAuto[slideIndexAuto - 1].style.display = "block";
-    dotsAuto[slideIndexAuto - 1].classList.add("active");
-    setTimeout(showSlidesAuto, 3000); // Change image every 2 seconds
-}
-//-------DUNG AUTO SLIDE KHI CLICK-------
-function stopAutoSlide() {
-    clearTimeout(autoSlideTimeout);
-}
-// Trang đăng kí và đăng nhập
-const title = document.querySelector(".title1 h1");
-const letter = "elcome !";
-title.textContent = "W";
-var index = 0;
-
-setInterval(() => {
-    title.textContent += letter[index];
-    index++;
-    if (index == letter.length + 1) {
-        index = 0;
-        title.textContent = "W";
-    }
-}, 500);
+//slider owl-carousel library
+$(document).ready(function(){
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+    });
+    //Tien toi slide tiep theo
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    //Lui ve slide truoc
+    $('.customPrevBtn').click(function() {
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+});
 
 // Trang đăng ký
 function checkNumber(event) {
